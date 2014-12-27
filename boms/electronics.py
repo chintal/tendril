01462d6c74f9eea494d8903dfe36783deb275461
Chintalagiri Shashank <shashank@chintal.in>
2014-12-27 23:04:26 +0530

    GedaProjectFile class added

--- boms/electronics.py
+++ boms/electronics.py
@@ -3,8 +3,9 @@ This file is part of koala
 See the COPYING, README, and INSTALL files for more information
 """
 import os
-import csv
 import logging
+import yaml
+
 
 class EntityBase(object):
     id = None
@@ -46,20 +47,58 @@ class EntityGroup(EntityBase):
 
 
 class EntityBomConf(EntityBase):
-    """A single BOM configuration
-
-    Information only class for a single configuration
-    of a single PCB.
+    """All allowed configurations for a PCB
 
     Attributes:
-        configname
         grouplist
-        parent_bom
+        configsections(L)
+            configsection
+            grouplist
+            configurations(L)
+                configname
+                groups
+        configurations(L)
+            configname
+            config
 
     """
-    def __init__(self):
+    def __init__(self, confdata):
         super(EntityBomConf, self).__init__()
+        self.grouplist = confdata["grouplist"]
+        self.configsections = confdata["configsections"]
+        self.configurations = confdata["configurations"]
+
+    def get_configurations(self):
+        rval = []
+        for configuration in self.configurations:
+            rval.append(configuration["configname"])
+        return rval
         pass
+
+    def get_configsections(self):
+        rval = []
+        for configsection in self.configsections:
+            rval.append(configsection["sectionname"])
+        return rval
+
+    def get_sec_groups(self, sectionname, config):
+        rval = []
+        for section in self.configsections:
+            if section["sectionname"] == sectionname:
+                for configuration in section["configurations"]:
+                    if configuration["configname"] == config:
+                        for group in configuration["groups"]:
+                            rval.append(group)
+        return rval
+
+    def get_configuration(self, configname):
+        rval = ["default"]
+        for configuration in self.configurations:
+            if configuration["configname"] == configname:
+                for configsection in self.get_configsections():
+                    sec_confname = configuration["config"][configsection]
+                    rval = rval + self.get_sec_groups(configsection, sec_confname)
+        return rval
 
 
 class EntityBom(EntityBase):
@@ -72,14 +111,26 @@ class EntityBom(EntityBase):
         grouplist
 
     """
-    def __init__(self):
+    def __init__(self, confdata, cardfolder):
         super(EntityBom, self).__init__()
+        self.pcbname = confdata["pcbname"]
+        self.projfile = confdata["projfile"]
+        self.pcbfolder = cardfolder
+        self.configurations = EntityBomConf(confdata)
         pass
 
 
-def import_pcb(pcbfolder):
-    pass
+def import_pcb(cardfolder):
+    pcbbom = None
+    with open(os.path.normpath(cardfolder+"/schematic/configs.yaml")) as configfile:
+        configdata = yaml.load(configfile)
+    if configdata["schema"]["name"] == "pcbconfigs" and \
+       configdata["schema"]["version"] == 1.0:
+        pcbbom = EntityBom(configdata, cardfolder)
+    else:
+        logging.ERROR("Config file schema is not supported")
+    return pcbbom
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
-    import_pcb("/home/chintal/code/koala/scratch")
+    data = import_pcb("/home/chintal/code/koala/scratch")
---
+++ gedaif/projfile.py
@@ -0,0 +1,34 @@
+"""
+This file is part of koala
+See the COPYING, README, and INSTALL files for more information
+"""
+
+import os
+
+
+class GedaProjectFile(object):
+
+    schfiles = []
+    pcbfile = None
+
+    def __init__(self, projectfolder, projfile):
+        projfilepath = os.path.normpath(projectfolder+"/schematic/"+projfile)
+        with open(projfilepath, 'r') as f:
+            for line in f:
+                line = self.strip_line(line)
+                if line != '':
+                    parts = line.split('\t')
+                    if parts[0].strip() == 'schematics':
+                        for part in parts[1].split(' '):
+                            self.schfiles.append(part.strip())
+                    if parts[0].strip('\t') == 'output-name':
+                        self.pcbfile = parts[1].strip().split('/')[-1]
+
+    @staticmethod
+    def strip_line(line):
+        line = line.split("#")[0]
+        return line.strip()
+
+
+if __name__=="__main__":
+    pass
