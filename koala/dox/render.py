e439593d8d9461cf4007ec99780c664f0b5c737f
Chintalagiri Shashank <shashank@chintal.in>
2015-07-29 00:56:37 +0530

    Added Licenses

--- koala/dox/render.py
+++ koala/dox/render.py
@@ -1,3 +1,19 @@
+# Copyright (C) 2015 Chintalagiri Shashank
+#
+# This file is part of Koala.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 Dox Render module documentation (:mod:`dox.render`)
 ===================================================
@@ -45,6 +61,8 @@ def jinja2_pdfinit():
                                   block_end_string='%}',
                                   variable_start_string='%{{',
                                   variable_end_string='%}}',
+                                  comment_start_string='%{#',
+                                  comment_end_string='%#}',
                                   loader=loader)
     renderer.filters['format_currency'] = format_currency
     renderer.filters['escape_latex'] = escape_latex
