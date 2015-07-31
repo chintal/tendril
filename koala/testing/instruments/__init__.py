27bd6ed842951345280745c6b88cf1dc6c927db5
Chintalagiri Shashank <shashank@chintal.in>
2015-07-31 05:45:04 +0530

    Fixed instrument import code

--- koala/testing/instruments/__init__.py
+++ koala/testing/instruments/__init__.py
@@ -1,17 +1,17 @@
 # Copyright (C) 2015 Chintalagiri Shashank
-# 
+#
 # This file is part of Koala.
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
@@ -39,7 +39,7 @@ INSTANCE_INSTRUMENTS = import_(INSTANCE_INSTRUMENTS_ROOT)
 def get_instrument_object(instst):
     modname = instst
     modstr = 'koala.testing.instruments.' + modname
-    clsname = 'Test' + instst
+    clsname = 'Instrument' + instst
     try:
         return INSTANCE_INSTRUMENTS.get_test_object(instst)
     except ValueError:
@@ -50,4 +50,4 @@ def get_instrument_object(instst):
         instance = cls()
         return instance
     except ImportError:
-        raise ValueError("Test Unrecognized :" + instst)
+        raise ValueError("Instrument Unrecognized :" + instst)
